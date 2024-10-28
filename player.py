13c089857b1ccdf0a7599661bc516405eb308bbc
inksaywhat <mcmaster9@gmail.com>
2024-10-28 10:45:47 -0600

    Add player movement functionality: Implement move method to handle forward and backward movement using W and S keys

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -21,12 +21,21 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt 
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)  # Move forward
+        if keys[pygame.K_s]:
+            self.move(-dt)  # Move backward
+
         if keys[pygame.K_a]:
-            self.rotate(-dt)
+            self.rotate(-dt)  # Rotate left
         if keys[pygame.K_d]:
-            self.rotate(dt)
+            self.rotate(dt)   # Rotate right
